981ebd447c67aebb5054301787262adc5e267d6a
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-04-02 03:02:32 +0430

    Add Titanic Competition

--- Titanic: Machine Learning From Disaster/network.py
+++ Titanic: Machine Learning From Disaster/network.py
@@ -3,10 +3,23 @@ import pandas as pd
 
 import tensorflow as tf
 import matplotlib.pyplot as plt
+
+"""
+    Created by Mohsen Naghipourfar on 4/2/18.
+    Email : mn7697np@gmail.com
+    Website: http://ce.sharif.edu/~naghipourfar
+"""
+
+import numpy as np
+import pandas as pd
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
 import keras
 from keras.layers import Dense, Input, Dropout
 from keras.models import Sequential
 from sklearn.preprocessing import normalize
+
 """
     Created by Mohsen Naghipourfar on 4/2/18.
     Email : mn7697np@gmail.com
@@ -18,7 +31,7 @@ TEST_PATH = './test.csv'
 
 # Hyper-Parameters
 n_features = 81
-n_epochs = 1000
+n_epochs = 10
 batch_size = 256
 
 
@@ -29,21 +42,19 @@ def load_data(filepath):
 def MLP_model():
     model = Sequential()
     # model.add(Input(shape=(n_features,)))
-    model.add(Dense(64, input_shape=(n_features,)))
+    model.add(Dense(64, activation='relu', input_shape=(n_features,)))
     model.add(Dropout(0.5))
-    model.add(Dense(32))
+    model.add(Dense(32, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(16))
+    model.add(Dense(16, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(8))
+    model.add(Dense(8, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(4))
+    model.add(Dense(4, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(2))
-    model.add(Dropout(0.5))
-    model.add(Dense(1))
+    model.add(Dense(2, activation='softmax'))
 
-    model.compile(optimizer='adam', loss='mse')
+    model.compile(optimizer='adam', loss='categorical_crossentropy')
 
     model.summary()
 
@@ -63,16 +74,19 @@ def predict(model, x_test):
 
 
 def build_submission(x_test, y_pred):
-    idx = x_test.columns.get_loc('Id')
+    idx = x_test.columns.get_loc('PassengerId')
     import csv
     with open('./submission.csv', 'w') as file:
         writer = csv.writer(file)
-        writer.writerow(['Id', 'SalePrice'])
-        for i in range(len(y_pred)):
-            writer.writerow([x_test.iloc[i, idx], y_pred[i][0]])
+        writer.writerow(['PassengerId', 'Survived'])
+        for i in range(y_pred.shape[0]):
+            writer.writerow([x_test.iloc[i, idx], np.argmax(y_pred[i])])
 
 
 def preprocess_data(data, test_data):
+    data = data.drop('Name', axis=1)
+    test_data = test_data.drop('Name', axis=1)
+
     # 1.1. Drop Columns which Contains Missing Values in all elements
     # data = data.dropna(axis=1, how='all')
     # 1. Drop All Missing Data is a Fantastic Way
@@ -83,8 +97,8 @@ def preprocess_data(data, test_data):
     # 2. Convert Categorical to Dummies
     n_train_samples = data.shape[0]
 
-    y_train = data['SalePrice']
-    train_data = data.drop('SalePrice', axis=1)
+    y_train = data['Survived']
+    train_data = data.drop('Survived', axis=1)
     all_data = train_data.append(test_data)
     all_data = pd.get_dummies(all_data)
 
@@ -106,9 +120,6 @@ def process_missing_data(data, test_data):
     data = data.drop(indices, 1)
     test_data = test_data.drop(indices, 1)
 
-    data = data.drop(data.loc[data['Electrical'].isnull()].index)
-
-    # data.isnull().sum().max()  # just checking that there's no missing data missing...
     return data, test_data
 
 
@@ -117,18 +128,14 @@ if __name__ == '__main__':
     test_data = load_data(TEST_PATH)
 
     x_train, y_train, x_test = preprocess_data(training_data, test_data)
-
-    # x_train = training_data.drop(labels=['SalePrice', 'Id'], axis=1)
-    # y_train = training_data['SalePrice']
-
-    # x_train = pd.DataFrame(normalize(x_train, axis=1))
+    y_train = pd.DataFrame(keras.utils.to_categorical(y_train.as_matrix(), 2))
 
     n_features = x_train.shape[1] - 1
 
     model = MLP_model()
 
-    train_model(model, x_train.drop('Id', axis=1).as_matrix(), y_train.as_matrix())
+    train_model(model, x_train.drop('PassengerId', axis=1).as_matrix(), y_train.as_matrix())
 
-    y_test = predict(model, x_test.drop('Id', axis=1).as_matrix())
+    y_test = predict(model, x_test.drop('PassengerId', axis=1).as_matrix())
 
-    build_submission(x_test, y_test.values.tolist())
+    build_submission(x_test, y_test)
